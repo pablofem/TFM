0633cd49ba106118205b27518008d8ce5561f7f0
pfmarroquin <pfmarroquin@eaglehubs.com>
2024-07-21 23:55:20 -0600

    [add] prediction datasets

--- modeling_auxiliary_functions.py
+++ modeling_auxiliary_functions.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import datetime as dt
+from tqdm import tqdm
 
 
 def apply_sin_cos_transform(column, column_name, max_val):
@@ -66,3 +68,56 @@ def create_weekly_features(df, target_date, weeks, columns):
     new_df = pd.DataFrame([new_row])
 
     return new_df
+
+
+def prepare_features_dataframe(df, start_timestamp, end_timestamp, execution_timestamp, weekly_info_length, hourly_info_length):
+    dr = pd.date_range(start_timestamp, end_timestamp, freq="H")
+    columns = df.columns.tolist()
+    columns.remove("time")
+
+    row_list = []
+    for date in dr:
+        date = date.to_pydatetime()
+        weekly_df = create_weekly_features(df, date, weekly_info_length, columns)
+        hourly_df = create_hourly_features(df, date, execution_timestamp, hourly_info_length, columns)
+        row_df = pd.merge(weekly_df, hourly_df, how="inner", on="time")
+        row_list.append(row_df)
+
+    predictor_dataframe = pd.concat(row_list)
+    return predictor_dataframe
+
+
+def get_predictions_timestamps(date, offer_type):
+
+    start_timestamp = date
+    end_timestamp = date + dt.timedelta(hours=23)
+    if offer_type == "market_offer":
+        execution_timestamp = date - dt.timedelta(hours=12)
+    elif offer_type == "first_session":
+        execution_timestamp = date - dt.timedelta(hours=1)
+    elif offer_type == "last_session":
+        execution_timestamp = date + dt.timedelta(hours=12)
+        start_timestamp = date + dt.timedelta(hours=13)
+
+    return start_timestamp, end_timestamp, execution_timestamp
+
+
+def prepare_predictor_dataframe(df, start_date, end_date, offer_type, weekly_info_length, hourly_info_length):
+    dr = pd.date_range(start_date, end_date, freq="D")
+
+    predictor_dataframe_list = []
+    for date in tqdm(dr):
+        date = date.to_pydatetime()
+        start_timestamp, end_timestamp, execution_timestamp = get_predictions_timestamps(date, offer_type)
+        predictor_dataframe = prepare_features_dataframe(df, start_timestamp, end_timestamp, execution_timestamp, weekly_info_length, hourly_info_length)
+        predictor_dataframe_list.append(predictor_dataframe)
+
+    train_predictor_dataframe = pd.concat(predictor_dataframe_list)
+    return train_predictor_dataframe
+
+
+def get_demand_df(interval_df, dataset):
+    interval_df = interval_df[["time"]]
+    dataset = dataset[["time", "total_load_actual"]]
+    demand_df = pd.merge(interval_df, dataset)
+    return demand_df
